88b0c45e17145dd07e6f4c1dfaeeaeda534799d5
Ganashree G <ganashreeg@gmail.com>
2020-07-28 18:36:26 +0530

    Initial commit. Programe run successfully

---
+++ src/main/java/com/ganashree/springbootreactivemongodb/SpringbootReactiveMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ganashree.springbootreactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactiveMongodbApplication.class, args);
+	}
+
+}
